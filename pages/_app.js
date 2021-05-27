4e15515b3d37e7b1902179d220fc8740739f9c6e
Sammie-drew <empireandz@gmail.com>
2021-05-28 00:06:54 +0100

    Strated Blog App

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,7 @@
-import Head from "next/head";
-import Layout from "../components/layout/layout";
-
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
-import { NotificationContextProvider } from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <NotificationContextProvider>
-      <Layout>
-        <Head>
-          <title>Next Event Center</title>
-          <meta name="description" content="NextJS Events" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <ToastContainer />
-        <Component {...pageProps} />
-      </Layout>
-    </NotificationContextProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,9 @@
-import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../helpers/api-utils";
-import Head from "next/head";
-import NewsletterRegistration from "../input/newsletter-registration";
-
-const HomePage = ({ featuredEvents }) => {
+const index = () => {
   return (
     <div>
-      <Head>
-        <title>Next js Event center</title>
-        <meta
-          name="description"
-          content="Contains a list of events available in next js"
-        />
-      </Head>
-      <NewsletterRegistration />
-      <EventList items={featuredEvents} />
+      <h2>Hello WOrld</h2>
     </div>
   );
 };
 
-export async function getStaticProps(context) {
-  const featuredEvents = await getFeaturedEvents();
-
-  return {
-    props: {
-      featuredEvents,
-    },
-    revalidate: 1800,
-  };
-}
-
-export default HomePage;
+export default index;
