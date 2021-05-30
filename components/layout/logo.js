d6787170d886f96880e6925f3ca7a37f99bd4238
Sammie-drew <empireandz@gmail.com>
2021-05-30 19:35:54 +0100

    New Project Started

--- components/layout/logo.js
+++
@@ -1,7 +0,0 @@
-import styles from "./logo.module.css";
-
-const Logo = () => {
-  return <div className={styles.logo}>Ether's Spaces</div>;
-};
-
-export default Logo;
--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,7 @@
-import Layout from "../components/layout/layout";
-import "../styles/globals.css";
-import Head from "next/head";
-
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-      <ToastContainer />
-    </Layout>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
